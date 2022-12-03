f2443b653851f6b0a2a7b52876d074948b20eebf
IRobin <in.robin12@gmail.com>
2022-12-03 13:20:44 +0000

    Day 2 part 2 solved

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -31,17 +31,65 @@ namespace AdventOfCode2022
 
                return theirMove == winCharacter ? 6 : 0;
             }
+
+            int GetRoundScore(bool win, bool draw)
+            {
+               switch (theirMove)
+               {
+                  case 'A':
+                     if (win)
+                     {
+                        return 2;
+                     } else if (draw)
+                     {
+                        return 1;
+                     }
+                     else
+                     {
+                        return 3;
+                     }
+                  case 'B':
+                     if (win)
+                     {
+                        return 3;
+                     } else if (draw)
+                     {
+                        return 2;
+                     }
+                     else
+                     {
+                        return 1;
+                     }
+                  case 'C':
+                     if (win)
+                     {
+                        return 1;
+                     } else if (draw)
+                     {
+                        return 3;
+                     }
+                     else
+                     {
+                        return 2;
+                     }
+               }
+
+               return 0;
+            }
             
             switch (myMove)
             {
                case 'X':
-                  scores[i] = 1 + GetOutcomeScore('A', 'C');
+                  // scores[i] = 1 + GetOutcomeScore('A', 'C');
+                  scores[i] = 0 + GetRoundScore(false, false);
                   break;
                case 'Y':
-                  scores[i] = 2 + GetOutcomeScore('B', 'A');
+                  //scores[i] = 2 + GetOutcomeScore('B', 'A');
+                  scores[i] = 3 + GetRoundScore(false, true);
                   break;
                case 'Z':
-                  scores[i] = 3 + GetOutcomeScore('C', 'B');
+                  //scores[i] = 3 + GetOutcomeScore('C', 'B');
+                  scores[i] = 6 + GetRoundScore(true, false);
                   break;
             }
          }
