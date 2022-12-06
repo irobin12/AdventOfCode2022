7675f296da842c7b53dd784f1970e6b67efabd20
IRobin <in.robin12@gmail.com>
2022-12-06 00:35:45 +0000

    Day 5 input processing 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -8,6 +8,20 @@ namespace AdventOfCode2022
 {
    internal class Program
    {
+      private class Instruction
+      {
+         public int amountToMove;
+         public int origin;
+         public int destination;
+
+         public Instruction(int amountToMove, int origin, int destination)
+         {
+            this.amountToMove = amountToMove;
+            this.origin = origin;
+            this.destination = destination;
+         }
+      }
+      
       public static void Main()
       {
          // Edit Configurations > Working directory to find the files path
@@ -61,12 +75,20 @@ namespace AdventOfCode2022
             stacks.Add(stacks.Count + 1, stack);
          }
 
-         Console.WriteLine("top");
+         Queue<Instruction> instructions = new Queue<Instruction>();
+
+         for (int i = indexOfEmptyLine + 1; i < input.Length - 1; i++)
+         {
+            var digits = Regex.Split(input[i], "\\D+").Where(s => s != String.Empty).ToArray();
+            instructions.Enqueue(new Instruction(int.Parse(digits[0]), int.Parse(digits[1]), int.Parse(digits[2])));
+         }
+
+         var t = 0;
          /*string stacksNumbers = input[indexOfEmptyLine - 1];
          int numberOfStacks = stacksNumbers[stacksNumbers.Length - 2];
          
          char[,] cargo = new char[indexOfEmptyLine - 2, numberOfStacks];*/
-         
+
          /*List<Stack<char>> stacks = new List<Stack<char>>();
          
          for (int index = startingLine; index >= 0; index--)
