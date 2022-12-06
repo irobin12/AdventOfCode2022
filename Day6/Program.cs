cb7e24350d311b63d0e4e00b03faa84980e50573
IRobin <in.robin12@gmail.com>
2022-12-06 22:27:52 +0000

    Day 6 Part 2 done??? too easy???

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -20,9 +20,9 @@ namespace AdventOfCode2022
 
          for (int i = 0; i < input.Length; i++)
          {
-            if (input.Substring(i, 4).Distinct().ToArray().Length == 4)
+            if (input.Substring(i, 14).Distinct().ToArray().Length == 14)
             {
-               Console.WriteLine(i + 4);
+               Console.WriteLine(i + 14);
                break;
             }
          }
