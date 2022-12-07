2111b6f3c0fbc6442e0ed96cb280f64a9439c7e0
IRobin <in.robin12@gmail.com>
2022-12-07 23:41:01 +0000

    Day 7 setup

---
+++ Day7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022
+{
+   internal class Program
+   {
+      public static void Main()
+      {
+         // Edit Configurations > Working directory to find the files path
+
+         string[] input = File.ReadAllLines("ExampleInput.txt");
+         //string[] input = File.ReadAllLines("Input.txt");
+         
+         //string input = File.ReadAllText("ExampleInput.txt");
+         //string input = File.ReadAllText("Input.txt");
+
+         
+      }
+   }
+}
