94e83d67084b66c9d5507de482774a8713c0eaa9
IRobin <in.robin12@gmail.com>
2022-12-10 13:47:26 +0000

    Day 7 correct input processing :D

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -8,22 +8,23 @@ namespace AdventOfCode2022
 {
    public class Node
    {
-      public string name;
-      public bool isDirectory;
-      public int size = 0;
+      public readonly string name;
+      public readonly bool isDirectory;
+      public int size;
       public List<Node> nodes = new List<Node>();
 
-      public Node(string name, bool isDirectory)
+      public Node(string name, bool isDirectory, int size = 0)
       {
          this.name = name;
          this.isDirectory = isDirectory;
+         this.size = size;
       }
    }
    
    internal class Program
    {
       private static Node root = new Node("/", true);
-      //private static List<Node> directories = new List<Node>();
+      private static string[] fileInput;
 
       public static void Main()
       {
@@ -35,68 +36,38 @@ namespace AdventOfCode2022
          //string input = File.ReadAllText("ExampleInput.txt");
          //string input = File.ReadAllText("Input.txt");
 
-         //directories.Add(root);
-         ProcessInput(input, root);
+         fileInput = input;
+         
+         ProcessInput(2, root);
       }
 
-      public static void ProcessInput(string[] input, Node parentNode)
+      private static void ProcessInput(int startIndex, Node parentNode)
       {
-         for (int i = 2; i < input.Length; i++)
+         for (int i = startIndex; i < fileInput.Length; i++)
          {
-            string line = input[i];
-            char firstChar = line[0];
+            string line = fileInput[i];
 
-            switch (firstChar)
+            switch (line[0])
             {
                case '$':
-                  if(line.Length < 6) break;
-                  var newDirectory = line.Substring(5);
-                  if(newDirectory == "..") return;
-                  var newParentNode = parentNode.nodes.Find(n => n.name == newDirectory);
-                  ProcessInput(input.Skip(i).ToArray(), newParentNode);
-                  break;
+                  goto ProcessDirectories;
                case 'd':
-                  var directoryNode = new Node(line.Substring(4), true);
-                  //if(directories.Contains<Node>(n => directoryNode.name != n.name)directories.Add(directoryNode));
-                  parentNode.nodes.Add(directoryNode);
+                  parentNode.nodes.Add(new Node(line.Substring(4), true));
                   break;
                default:
                   int lastDigitIndex = Array.FindLastIndex(line.ToCharArray(), char.IsDigit);
-                  var node = new Node(line.Substring(lastDigitIndex + 2), false);
-                  node.size = Int32.Parse(line.Substring(0, lastDigitIndex + 1));
-                  parentNode.nodes.Add(node);
+                  parentNode.nodes.Add(new Node(line.Substring(lastDigitIndex + 2), false, Int32.Parse(line.Substring(0, lastDigitIndex + 1))));
                   break;
             }
-            
-            /*if(firstChar == '$') break;
-            if (firstChar == 'd')
-            {
-               var node = new Node(line.Substring(4), true);
-            }
-            else
-            {
-               //int lastDigitIndex = FindLastDigitIndex(line);
-               int lastDigitIndex = Array.FindLastIndex(line.ToCharArray(), char.IsDigit);
-
-               var node = new Node(line.Substring(lastDigitIndex + 2), false);
-               node.size = Int32.Parse(line.Substring(0, lastDigitIndex + 1));
-            }*/
          }
-      }
 
-      private static int FindLastDigitIndex(string line)
-      {
-         //int lastDigitIndex = line.LastIndexOf(c => Char.IsDigit(c));
-         //int index = Array.FindLastIndex(line.ToCharArray(), char.IsDigit);
-
-         int lastDigitIndex = -1;
-         foreach (char c in line)
+         ProcessDirectories:
+         foreach (Node node in parentNode.nodes.Where(node => node.isDirectory))
          {
-            if (Char.IsDigit(c)) lastDigitIndex++;
-            else break;
+            string commandLine = "$ cd " + node.name;
+            int index = Array.FindIndex(fileInput, s => s == commandLine);
+            ProcessInput(index + 2, node);
          }
-
-         return lastDigitIndex;
       }
 
       public int GetTotalDirectoriesSum()
