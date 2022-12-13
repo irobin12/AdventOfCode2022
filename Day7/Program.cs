5525df61e45bb63e561a85f3e694a3120c886cc5
IRobin <in.robin12@gmail.com>
2022-12-13 21:38:57 +0000

    This doesn't work. There's recursion. I'll do it all over again.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -64,47 +64,10 @@ namespace AdventOfCode2022
          for (int i = 0; i < fileInput.Length; i++)
          {
             string line = fileInput[i];
-            
-            List<Node> GetChildren()
-            {
-               List<Node> children = new List<Node>();
-               
-               for (int j = i + 2; j < fileInput.Length; j++)
-               {
-                  string nextLine = fileInput[j];
-
-                  switch (nextLine[0])
-                  {
-                     case '$':
-                        goto End;
-                     case 'd':
-                        string directoryName = nextLine.Substring(5);
-                        Node childDirectory = directories.Find(d => d.name == directoryName);
-                        if(childDirectory == null)
-                        {
-                           //childDirectory = new Node(directoryName, true, GetChildren());
-                           directories.Add(childDirectory);
-                        }
-                        children.Add(childDirectory);
-                        break;
-                     default:
-                        int lastDigitIndex = Array.FindLastIndex(line.ToCharArray(), char.IsDigit);
-                        children.Add(new Node(line.Substring(lastDigitIndex + 2), false, size:Int32.Parse(line.Substring(0, lastDigitIndex + 1))));
-                        break;
-                  }
-               }
-               
-               End:
-               return children;
-            }
-
             if (line.Substring(0, 3) == "$ c" && line[line.Length - 1] != '.')
             {
                string name = line.Substring(5);
-               if(directories.Find(d => d.name == name) == null)
-               {
-                  directories.Add(new Node(name, true, indexInInput:i));
-               }
+               directories.Add(new Node(name, true, indexInInput:i));
             }
          }
       }
@@ -113,18 +76,32 @@ namespace AdventOfCode2022
       {
          foreach (Node directory in directories)
          {
-            directory.children = new List<Node>();
+            int lastIndex = directory.indexInInput + 2;
             for (int i = directory.indexInInput + 2; i < fileInput.Length; i++)
+            {
+               if (fileInput[i][0] == '$')
+               {
+                  lastIndex = i - 1;
+                  break;
+               }
+
+               if (i == fileInput.Length - 1)
+               {
+                  lastIndex = i;
+                  break;
+               }
+            }
+
+            directory.children = new List<Node>();
+            for (int i = directory.indexInInput + 2; i <= lastIndex; i++)
             {
                string line = fileInput[i];
 
                switch (line[0])
                {
-                  case '$':
-                     goto End;
                   case 'd':
                      string directoryName = line.Substring(4);
-                     Node childDirectory = directories.Find(d => d.name == directoryName);
+                     Node childDirectory = directories.Find(d => d.name == directoryName); // cause recursion
                      directory.children.Add(childDirectory);
                      break;
                   default:
@@ -133,15 +110,15 @@ namespace AdventOfCode2022
                      break;
                }
             }
-            
-            End: ;
          }
       }
 
       private static void CalculateDirectoriesSizes()
       {
-         foreach (Node directory in directories)
+         for (int index = 0; index < directories.Count; index++)
          {
+            Node directory = directories[index];
+            Console.WriteLine($"Calculating size of directory {directory.name} at index {index}.");
             if (directory.size != 0) continue;
             CalculateDirectorySize(directory);
          }
